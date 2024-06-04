003b566fbca4e7d2d51f86d3f65ae1d777075ae7
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-06-04 11:38:24 +0530

    Time: 5 ms (66.65%) | Memory: 42 MB (17.96%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,36 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int longestPalindrome(String s) {
-        int result = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+        int len = 0;
 
-        // Count the frequency of each character
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
-
-        boolean oddFound = false;
-
-        // Calculate the length of the longest palindrome
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            int value = entry.getValue();
-            
-            // Add the even part of the frequency
-            result += value / 2 * 2;
-            
-            // Check if there is any odd frequency to add one as the center of the palindrome
-            if (value % 2 == 1) {
-                oddFound = true;
+        for (char c : s.toCharArray()){
+            if (set.contains(c)) {
+                set.remove(c);
+                len+=2;
+            } else {
+                set.add(c);
             }
         }
 
-        // If there is an odd frequency, add one to the result
-        if (oddFound) {
-            result += 1;
+        if(!set.isEmpty()) {
+            return len+1;
         }
-
-        return result;
+        return len;
     }
 }
