92156b8bbc4a5cd1131e6019f24b7670e57ebf5c
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-06-15 08:52:23 +0530

    Time: 3 ms (89.91%) | Memory: 42.8 MB (85.83%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -5,25 +5,32 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
         ArrayList<Integer> result = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        // Count occurrences of each number in nums1
-        for (int i = 0; i < nums1.length; i++){
-            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
-        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        // Check for intersections with nums2 and update the map
-        for (int x : nums2){
-            if(map.containsKey(x) && map.get(x) > 0){
-                result.add(x);
-                map.put(x, map.get(x) - 1); // Remove one occurrence from the map
+        int i = 0;
+        int j = 0;
+
+        while (i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else if (nums1[i] == nums2[j]){
+                result.add(nums1[i]);
+                i++;
+                j++;
             }
         }
+        
+        
 
         // Convert the result list to an array
         int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++){
-            answer[i] = result.get(i);
+        for (int f = 0; f < result.size(); f++){
+            answer[f] = result.get(f);
         }
 
         return answer;
