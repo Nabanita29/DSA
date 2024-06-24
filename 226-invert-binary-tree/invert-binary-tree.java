9d2d388b3c92f7e4e2bc1893d99f68618cac3e09
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-06-24 20:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (81.74%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // Swap the left and right children
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        // Recursively invert the children
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return root;
+    }
+}
