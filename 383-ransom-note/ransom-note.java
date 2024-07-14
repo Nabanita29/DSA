ae96919889edb7f47d4ff663ea4e74e67d999dcd
Nabanita <107246882+Nabanita29@users.noreply.github.com>
2024-07-14 10:53:26 +0530

    Time: 2 ms (79.70%) | Memory: 44.6 MB (72.88%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,30 +1,21 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // Create a HashMap to store the frequency of each character in the magazine
-        HashMap<Character, Integer> charCountMap = new HashMap<>();
-
-        // Populate the HashMap with the frequency of each character in the magazine
-        for (char c : magazine.toCharArray()) {
-            // If the character is already in the map, increment its count by 1
-            // If not, add it to the map with a count of 1
-            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+         // Create an array to count the frequency of each character in the magazine
+        int[] charCount = new int[26];
+        
+        // Count the frequency of each character in the magazine
+        for (char ch : magazine.toCharArray()) {
+            charCount[ch - 'a']++;
         }
-
-        // Check each character in the ransom note
-        for (char c : ransomNote.toCharArray()) {
-            // If the character is in the map and its count is greater than 0
-            if (charCountMap.containsKey(c) && charCountMap.get(c) > 0) {
-                // Decrease the count of the character in the map by 1
-                charCountMap.put(c, charCountMap.get(c) - 1);
-            } else {
-                // If the character is not in the map or its count is 0, return false
-                return false;
+        
+        // Check if ransomNote can be constructed
+        for (char ch : ransomNote.toCharArray()) {
+            if (charCount[ch - 'a'] == 0) {
+                return false;  // Not enough characters in magazine
             }
+            charCount[ch - 'a']--;  // Use up one character
         }
-
-        // If we can match all characters in the ransom note with those in the magazine, return true
-        return true;
+        
+        return true;  // All characters in ransomNote are matched
     }
 }
